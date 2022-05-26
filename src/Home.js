c2fd7bf610929f92f1f1bccc7a049e219a2d93da
R.J. Freund <robert.joseph.freund@gmail.com>
2022-05-25 23:20:09 -0500

    minor changes

--- src/Home.js
+++ src/Home.js
@@ -3,60 +3,103 @@ import React, { useState, useEffect } from 'react';
 const Home = () => {
     const [err, setErr] = useState(null);
     const [isLoaded, setIsLoaded] = useState(false);
-    const [activity, setActivity] = useState(null);
+    const [activities, setActivities] = useState([]);
 
-    useEffect(()=>{
+    useEffect(() => {
         requestActivity();
     }, [])
 
-    function requestActivity(){
+    function requestActivity() {
         fetch("http://www.boredapi.com/api/activity/")
             .then(res => res.json())
             .then(
                 (response) => {
                     setIsLoaded(true);
-                    setActivity(response);
+                    setActivities([].concat(response, activities));
                 },
                 (error) => {
                     setIsLoaded(true);
                     setErr(error);
                 }
             )
-    }    
+    }
 
     if (err) {
         return <div>Error: {err.message}</div>;
     } else if (!isLoaded) {
         return <div>Loading...</div>;
     } else {
-        return <div>
-            { JSON.stringify(activity) }
-            <button className="btn btn-primary" type="submit" onClick={requestActivity}>Request Random Activity</button>
-            <dl className="row">
-                <dt className="col-sm-3">Description lists</dt>
-                <dd className="col-sm-9">A description list is perfect for defining terms.</dd>
+        return <div className="container">
+            <p className='text-center'>{JSON.stringify(activities)}</p>
+            <button className="btn btn-primary d-block mx-auto" type="submit" onClick={requestActivity}>Request Random Activity</button>
+            <div className="accordion" id="accordion">
+                {
+                    activities.map((activity, index) =>
+                        <div className="accordion-item" key={activity.activity}>
+                            <h2 className="accordion-header" id={"heading" + index}>
+                                <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target={"#collapse" + index} aria-expanded="true" aria-controls="collapseOne">
+                                    {activity.activity}
+                                </button>
+                            </h2>
+                            <div id={"collapse" + index} className={`accordion-collapse collapse ${(index==0)?"show":"hide"}`} aria-labelledby={"heading" + index} data-bs-parent="#accordion">
+                                <div className="accordion-body">
+                                    <dl key={activity.activity} className="row justify-content-md-center">
+                                        <dt className="col-sm-6">Description</dt>
+                                        <dd className="col-sm-6">{activity.activity}</dd>
+                                        <dt className="col-sm-6">Type</dt>
+                                        <dd className="col-sm-6">{activity.type}</dd>
+                                        <dt className="col-sm-6">Number of participants</dt>
+                                        <dd className="col-sm-6">{activity.participants}</dd>
+                                        <dt className="col-sm-6">Relative price</dt>
+                                        <dd className="col-sm-6">{activity.price}</dd>
+                                        {activity.link &&
+                                        <>
+                                            <dt className="col-sm-6">Link</dt>
+                                            <dd className="col-sm-6">{activity.link}</dd>
+                                        </>
+                                        }
+                                        <dt className="col-sm-6">Key</dt>
+                                        <dd className="col-sm-6">{activity.key}</dd>
+                                        <dt className="col-sm-6">Accessibility rating</dt>
+                                        <dd className="col-sm-6">{activity.accessibility}</dd>
+                                    </dl>
+                                </div>
+                            </div>
+                        </div>
 
-                <dt className="col-sm-3">Term</dt>
-                <dd className="col-sm-9">
-                    <p>Definition for the term.</p>
-                    <p>And some more placeholder definition text.</p>
-                </dd>
-
-                <dt className="col-sm-3">Another term</dt>
-                <dd className="col-sm-9">This definition is short, so no extra paragraphs or anything.</dd>
-
-                <dt className="col-sm-3 text-truncate">Truncated term is truncated</dt>
-                <dd className="col-sm-9">This can be useful when space is tight. Adds an ellipsis at the end.</dd>
-
-                <dt className="col-sm-3">Nesting</dt>
-                <dd className="col-sm-9">
-                    <dl className="row">
-                    <dt className="col-sm-4">Nested definition list</dt>
-                    <dd className="col-sm-8">I heard you like definition lists. Let me put a definition list inside your definition list.</dd>
-                    </dl>
-                </dd>
-                </dl>
-            <Accordion />
+                    )
+                }
+                <div class="accordion-item">
+                    <h2 class="accordion-header" id="flush-headingOne">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+                            Accordion Item #1
+                        </button>
+                    </h2>
+                    <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
+                        <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the first item's accordion body.</div>
+                    </div>
+                </div>
+                <div class="accordion-item">
+                    <h2 class="accordion-header" id="flush-headingTwo">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
+                            Accordion Item #2
+                        </button>
+                    </h2>
+                    <div id="flush-collapseTwo" class="accordion-collapse collapse" aria-labelledby="flush-headingTwo" data-bs-parent="#accordionFlushExample">
+                        <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the second item's accordion body. Let's imagine this being filled with some actual content.</div>
+                    </div>
+                </div>
+                <div class="accordion-item">
+                    <h2 class="accordion-header" id="flush-headingThree">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
+                            Accordion Item #3
+                        </button>
+                    </h2>
+                    <div id="flush-collapseThree" class="accordion-collapse collapse" aria-labelledby="flush-headingThree" data-bs-parent="#accordionFlushExample">
+                        <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the third item's accordion body. Nothing more exciting happening here in terms of content, but just filling up the space to make it look, at least at first glance, a bit more representative of how this would look in a real-world application.</div>
+                    </div>
+                </div>
+            </div>
         </div>
         {/*
         return <div>
@@ -71,45 +114,5 @@ const Home = () => {
     */}
     }
 }
-
-const Accordion = () =>
-    <div className="accordion" id="accordionExample">
-        <div className="accordion-item">
-            <h2 className="accordion-header" id="headingOne">
-                <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                    Accordion Item #1
-                </button>
-            </h2>
-            <div id="collapseOne" className="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
-                <div className="accordion-body">
-                    <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-                </div>
-            </div>
-        </div>
-        <div className="accordion-item">
-            <h2 className="accordion-header" id="headingTwo">
-                <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                    Accordion Item #2
-                </button>
-            </h2>
-            <div id="collapseTwo" className="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
-                <div className="accordion-body">
-                    <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-                </div>
-            </div>
-        </div>
-        <div className="accordion-item">
-            <h2 className="accordion-header" id="headingThree">
-                <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                    Accordion Item #3
-                </button>
-            </h2>
-            <div id="collapseThree" className="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
-                <div className="accordion-body">
-                    <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-                </div>
-            </div>
-        </div>
-    </div>
 
 export default Home;
